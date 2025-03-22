37ff52a6fb734cfe1a4719a7f23ae83685ed3c92
Milo <172209513+Justmilomb@users.noreply.github.com>
2025-03-22 00:40:28 +0000

    Started making weather

---
+++ Weather/Weather.py/weather.py
@@ -0,0 +1,20 @@
+import requests
+if __name__ == "__main__":
+    print("Hello, World!")
+
+API_KEY = "24d1be9d1577492c7b63ea4adfe65877" 
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+city = input("Enter a city name:  ").strip().upper()
+params = {"q": city, "appid": API_KEY, "units": "metric" }
+response = requests.get(BASE_URL, params = params)
+if response.status_code == 200:
+	data = response.json()
+	temp =  data["main"]["temp"]
+	weather = data["weather"][0]["description"]
+	humidity = data["main"]["humidity"]
+	print(f"Weather in {city}:  ")
+	print(f"Temperature:  {temp}")
+	print(f"Condition:  {weather.capitalize()}")
+	print(f"humidity:  {humidity}%")
+else:
+	print("Error: API issue...")
